e26543e54da4774fd7e3e408022ff3c859b237f3
kratos1083 <36473951+kratos1083@users.noreply.github.com>
2018-02-15 18:19:14 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ jQuery(function($) {'use strict';
 		$(document).ready(function() {
     $('iframe#iframe').load(function() {
         var frame = $('iframe#iframe').contents();
-        console.log(frame);
+	    frame.find(".freebirdAccentBackground").css("background-color", "black");
     });
 });
 
